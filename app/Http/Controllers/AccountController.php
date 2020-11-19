99fd667a8f076b0f6cbbb6aea1915bdcfaabf2bb
MedAkou <akoutoss.med@gmail.com>
2020-11-19 14:45:17 +0100

    first commit

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\{Product,Slider,WishList,Addresses,Orders,User};
+use App;
+use Session;
+use Auth;
+use Validator;
+use Hash;
+use Request as req;
+use hisorange\BrowserDetect\Parser as Browser;
+use \Carbon\Carbon;
+
+class AccountController extends Controller {
+
+	public function __construct(){
+		if (!Auth::check()) {
+    		//return response()->view($this->theme.'account.user')->send();
+		}
+
+	}
+
+	public function edit() {
+        return view ($this->theme.'account.edit');
+    }
+
+    public function orders() {
+        return view ($this->theme.'account.orders');
+    }
+
+    public function order_detail($id) {
+
+    $content = Orders::find($id);
+    return view($this->theme.'account.order_detail',compact('content'));
+    }
+
+    public function wishlist() {
+        $wishlist = WishList::currentuser()->paginate(5);        
+        return view ($this->theme.'account.wish-list',compact('wishlist')) ;   
+    }
+
+	public function forgot()
+    {
+        return view ($this->theme.'forgot');
+    }
+
+    public function info() {
+     return view ($this->theme.'info');
+    }
+
+    public function adresses(){
+        return view ($this->theme.'account.adresses');    
+    }
+
+    public function edit_shipping( $id) {
+            $address = Addresses::find($id);
+            return view ($this->theme.'account.update-adress',compact('address'));   
+    }
+
+    public function update_shipping($id,Request $request) {
+
+            $address = Addresses::find($id);
+            $address->given_name   = $request->given_name;
+            $address->country_code = $request->country_code;
+            $address->street       = $request->street;
+            $address->state        = $request->state;
+            $address->city         = $request->city;
+            $address->housenumber  = $request->housenumber;
+            $address->postal_code  = $request->postal_code;
+            $address->phone        = $request->phone;
+            $address->save();
+            
+            return redirect()->route('account.adresses')->with('success',trans('user.shipping.updated'));
+    }
+
+    public function shipping_delete($id) {
+            Addresses::find($id)->delete();
+            return redirect()->route('account.adresses')->with('success',trans('user.shipping.removed'));
+    }
+
+    public function shipping_default($id){
+        $user_id   = Auth::user()->id;
+        $shippping = Addresses::where('user_id',$user_id)->where('is_primary',true)->first();
+        if($shippping) {
+            $shippping->is_primary= false;
+            $shippping->save();
+        }
+        $hop = Addresses::find($id);
+        $hop->is_primary= true;
+        $hop->save();                               
+        return redirect()->route('account.adresses')->with('success',trans('user.shipping.default'));
+    }
+
+    public function shipping_add() {
+            return view ($this->theme.'account.shipping_add')  ;
+        }
+
+    public function shipping_store(Request $request ) {
+
+            $adresse = [
+                'given_name'   => $request->given_name,
+                'country_code' => $request->country_code,
+                'street'       => $request->street,
+                'state'        => $request->state,
+                'housenumber'  => $request->housenumber,
+                'city'         => $request->city,
+                'postal_code'  => $request->postal_code,
+                'phone'        => $request->phone,
+                'user_id'      => Auth::user()->id,
+            ];
+
+            Addresses::create($adresse);
+
+            return redirect()->route('account.adresses')->with('success',trans('user.shipping.created'));
+        }
+
+    public function user(Request $request){
+        return view ($this->theme.'account.user') ;   
+    }
+
+    public function  userAuth(Request $request) {
+
+
+
+        if(!$request->filled('username') and !$request->filled('password') ){
+            return redirect()->route('user')->withInput()->with('error',trans('user.wrong.auth'));
+        }
+
+        $username = $request->username;
+        $password = $request->password;
+
+        $user = User::where('email',$username)->where('role','!=','manager')->where('role','!=','merchant')->where('statue','!=','blocked')->first();
+
+
+        if(!$user){
+
+        }
+
+        if (Auth::attempt(['email' => $username, 'password' => $password])){
+            $id_user = Auth::user()->id;
+            $lastlogin = User::find($id_user);
+            $lastlogin->last_login = Carbon::now();
+            $lastlogin->save(); 
+
+            return redirect('/');   
+        }
+
+        else {        
+             return redirect()->route('account.user')->with('error',trans('user.wrong.auth'));
+        } 
+        
+    }
+
+    public function  registration(Request $request) {
+
+        $geo = geoip(req::ip());
+
+        $rules = [
+          'email' => 'required|email|unique:users', 
+          'password' => 'required|min:3',
+          'name' => 'required|string|min:4',
+        ];
+
+        $request->validate($rules);
+
+        $user           =  new User();
+        $user->password =  Hash::make($request->password);
+        $user->email    =  $request->email;
+        $user->name     =  $request->name;
+        $user->ip       =  \Request::ip();
+        $user->device   =  Browser::platformName();
+        $user->browser  =  Browser::browserFamily();
+        $user->country  =  $geo['country'];
+        $user->statue   =  'active';
+        $user->last_login = Carbon::now();
+
+        if ($request->filled('phone')) {
+            $user->phone = $request->phone;
+        }
+        $user->save();
+
+        Auth::loginUsingId($user->id);
+
+        return redirect('/');       
+    }
+
+    public function  update(Request $request) {
+        $user = Auth::user();
+        $validator = Validator::make($request->all(), [
+          'email' => 'required|email|unique:users,email,'.$user->id, 
+          'name' => 'required|string|min:4',
+        ]);
+
+        if (!$validator->fails()) {
+            $user->email = $request->email;
+            $user->name = $request->name;
+            if ($request->filled('phone')) {
+                $user->phone = $request->phone;
+            }
+            $user->save();
+            return redirect()->route('account.edit')->with('success',trans('user.account.updated'));
+        }
+        return redirect()->route('account.user');  
+
+    }
+
+    public function password() {
+        return view ($this->theme.'account.password');
+    }
+
+    public function passwordUpdate(Request $request) {
+
+        $user = $request->user();
+
+        $validator = Validator::make($request->all(), [
+          'password' => 'required|min:3',
+          'newpassword' => 'required|min:3',
+          'password_confirmation' => 'required|min:3',
+        ]);
+
+        if($request->newpassword == $request->password_confirmation ) {
+            if (Hash::check($request->password, $user->password)) {
+                $user->password = Hash::make($request->newpassword);
+                $user->save();
+            }
+
+            return redirect()->route('account.password')->with('success',trans('user.pwd.updated'));
+        }
+
+        return redirect()->route('account.password')->with('error','wrong password');
+    }
+
+    public function clearwishlist(Request $request) {
+       $user = Auth::user();
+       $user->wishlist->each->delete();
+       return redirect()->route('account.wishlist')->with('success',trans('wishlist.cleared'));   
+    }
+  
+    
+
+}
---
+++ resources/views/theme2/account/wish-list.blade.php
@@ -0,0 +1,66 @@
+@extends('theme2/pages-layout')
+@section('title')
+{{ __('wishlist') }}
+@endsection
+
+@section('content')
+
+
+<main class="main">
+            <nav aria-label="breadcrumb" class="breadcrumb-nav">
+                <div class="container">
+                     <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="/"><i class="icon-home"></i></a></li>
+                        <li class="breadcrumb-item">{{ __('account') }}</li>
+                        <li class="breadcrumb-item active" aria-current="page">{{ __('wishlist') }}</li>
+                    </ol> 
+                </div>
+            </nav>
+
+            <div class="container">
+                <div class="row">                    
+                        @include('theme2.account.elements.sidebar')
+                        <div class="col-lg-8">
+                            <div class="ps-section__right">
+                                <div class="ps-section--account-setting">
+                                    <div class="ps-section__header">
+                                        <h3>{{ __('Wishlist') }}</h3>
+                                    </div>
+                                    <div class="ps-section__content">
+                                     <div class="table-responsive">
+                                         <table class="table ps-table--whishlist">
+                                             
+                                             <tbody>
+                                                @foreach($wishlist as $product)
+                                                 <tr>
+                                                     <td>
+                                                         <div class="ps-product--cart">
+                                                             <div class="ps-product__thumbnail"><a href="#">{!! $product->product->presentThumbnail() !!}</a></div>
+                                                             <div class="ps-product__content"><a href="#">{{$product->product->name }}</a></div>
+                                                         </div>
+                                                     </td>
+                                                     <td class="price">{{ $symbol }} {{ $product->product->presentPrice() }}</td>
+                                                 </tr>
+                                                 @endforeach
+                                             </tbody>
+                                             <tbody>
+                                                 <tr class="wishclearall">
+                                                    <th colspan="4"><a href="{{ route('account.wishlist_clear') }}"><i class="fa fa-trash" aria-hidden="true"></i>
+{{ __('clear wishlist') }}</a></th>
+                                                 </tr>
+                                             </tbody>
+                                         </table>
+                                     </div>
+                                 </div>
+                                </div>
+                            </div>
+                         </div>
+                </div>
+            </div>
+
+            <div class="mb-5"></div>
+        </main>
+
+
+
+@endsection
