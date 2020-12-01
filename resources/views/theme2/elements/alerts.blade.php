f19bef3fbd01210e534b23fdb7ee5a147ae00550
faissaloux <fwahabali@gmail.com>
2020-12-01 18:40:24 +0100

    fix: bugs

--- resources/views/theme2/elements/alerts.blade.php
+++ resources/views/theme2/elements/alerts.blade.php
@@ -5,3 +5,14 @@
 @if (session()->has('error'))
 		<div class="alert alert-error d-flex justify-content-center"> {{ session('error') }}	</div>
 @endif
+
+
+
+
+@if ($errors->any())
+<div class="alert alert-error d-flex justify-content-center">
+     @foreach ($errors->all() as $error)
+        {{$error}} &nbsp;
+     @endforeach
+     	</div>
+ @endif
